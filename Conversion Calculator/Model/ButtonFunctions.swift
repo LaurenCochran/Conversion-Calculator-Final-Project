2efbcbf2d76f4224a1c1101fb3c824f804b4500c
Lauren <Owner1@MacBooks-MacBook-Pro.local>
2019-05-06 20:33:05 -0500

    created a m a model

---
+++ Conversion Calculator/Model/ButtonFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonFunctions.swift
+//  Conversion Calculator
+//
+//  Created by Lauren on 2019-05-06.
+//  Copyright © 2019 Lauren Cochran. All rights reserved.
+//
+
+import Foundation
---
+++ Conversion Calculator/Model/Conversions.swift
@@ -0,0 +1,9 @@
+//
+//  Conversions.swift
+//  Conversion Calculator
+//
+//  Created by Lauren on 2019-05-06.
+//  Copyright © 2019 Lauren Cochran. All rights reserved.
+//
+
+import Foundation
